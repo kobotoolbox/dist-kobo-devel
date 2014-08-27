ad4b30a61c9ad562980b38ca8bd8c9f82c0b5a8b
Esmail <esmail.fadae@kobotoolbox.org>
2014-08-27 16:11:06 -0400

    'apt' needed keys for MongoDB repository.

--- scripts/02_installation_keys.sh
+++ scripts/02_installation_keys.sh
@@ -14,9 +14,11 @@ echo "KEYS >"
 
 export DEBIAN_FRONTEND=noninteractive
 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
 sudo add-apt-repository -y ppa:chris-lea/node.js
 
 echo "KEYS > All keys added '$VHOME'"
 touch $HOME/.mark_keys_added
+
