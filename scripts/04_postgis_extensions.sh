57766be7b40cc364a6beb239207e32d31682a49d
Esmail <esmail.fadae@kobotoolbox.org>
2014-08-27 16:13:33 -0400

    No need for PostgreSQL 'apt' repository.

--- scripts/04_postgis_extensions.sh
+++ scripts/04_postgis_extensions.sh
@@ -13,11 +13,5 @@
 sudo apt-get update
 
 sudo apt-get -y install qgis python-qgis
-
-# deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-
-sudo apt-get update
-sudo apt-get -y upgrade
 sudo apt-get -y install postgresql-9.3
 sudo apt-get -y install postgresql-9.3-postgis-2.1
