973d6859a07518be069514e460c3e1929265e70f
Esmail <esmail.fadae@kobotoolbox.org>
2014-08-27 16:15:19 -0400

    Use the 'postgres' user's administrative role for creating and setting DB.

--- scripts/05_create_database.sh
+++ scripts/05_create_database.sh
@@ -11,10 +11,10 @@ set -e
 
 install_info "Creating postgres database"
 
-if [ $(psql -l | grep kobotoolbox | wc -l) = "0" ]; then
-	createdb kobotoolbox
-	psql -d template1 -c "CREATE USER kobo WITH PASSWORD 'kobo';"
-	psql -d template1 -c "GRANT ALL PRIVILEGES ON DATABASE kobotoolbox to kobo;"
-	psql -c "CREATE EXTENSION IF NOT EXISTS postgis;" -d kobotoolbox
-	psql -c "CREATE EXTENSION IF NOT EXISTS postgis_topology;" -d kobotoolbox
+if [ $(sudo -u postgres psql -l | grep kobotoolbox | wc -l) = "0" ]; then
+	sudo -u postgres createdb kobotoolbox
+	sudo -u postgres psql -d template1 -c "CREATE USER kobo WITH PASSWORD 'kobo';"
+	sudo -u postgres psql -d template1 -c "GRANT ALL PRIVILEGES ON DATABASE kobotoolbox to kobo;"
+	sudo -u postgres psql -c "CREATE EXTENSION IF NOT EXISTS postgis;" -d kobotoolbox
+	sudo -u postgres psql -c "CREATE EXTENSION IF NOT EXISTS postgis_topology;" -d kobotoolbox
 fi
