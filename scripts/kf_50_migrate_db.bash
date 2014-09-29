40bb70cd13ca2edb3c92e8ef801489ca5500e286
Kobo Test Box <kobotestbox@kbtdev.org>
2014-09-29 14:00:13 -0400

    checks to see if user exists; creates if it doesnt

--- scripts/kf_50_migrate_db.bash
+++ scripts/kf_50_migrate_db.bash
@@ -20,8 +20,8 @@ cd $KOBOFORM_PATH
 workon kf
 
 python manage.py syncdb --noinput
-# python manage.py migrate --noinput
+python manage.py migrate --noinput
 
-# echo "from django.contrib.auth.models import User; print 'UserExists' if User.objects.filter(username='$DEFAULT_KOBO_USER') > 0 else User.objects.create_superuser('$DEFAULT_KOBO_USER', 'kobo@example.com', '$DEFAULT_KOBO_PASS');" | python manage.py shell 2>&1
+echo "from django.contrib.auth.models import User; print 'UserExists' if User.objects.filter(username='$DEFAULT_KOBO_USER') > 0 else User.objects.create_superuser('$DEFAULT_KOBO_USER', 'kobo@example.com', '$DEFAULT_KOBO_PASS');" | python manage.py shell 2>&1
 
 exit 0
