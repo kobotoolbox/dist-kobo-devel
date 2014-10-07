34e9e8bcdb8047259e90450e5ad0019e21445e7e
Kobo Test Box <kobotestbox@kbtdev.org>
2014-10-07 14:18:57 -0400

    fixing error where crontab was not set

--- scripts/09_add_cronjobs.sh
+++ scripts/09_add_cronjobs.sh
@@ -20,11 +20,6 @@ install_info "Adding cron jobs."
 BOOT_LAUNCH_SCRIPT="$HOME_VAGRANT/boot_launch_servers.sh"
 cp $V_S/X_boot_launch_servers.sh $BOOT_LAUNCH_SCRIPT
 
-existing_crontab="$(crontab -l)" 2> /dev/null
-if [ $(echo $existing_crontab | grep boot_launch | wc -l ) = "0" ]; then
-	echo $existing_crontab | { cat; echo "@reboot sh $BOOT_LAUNCH_SCRIPT &"; } | crontab -
-	install_info "crontab added: '@reboot sh $BOOT_LAUNCH_SCRIPT &'"
-	sh $BOOT_LAUNCH_SCRIPT
-else
-	echo "crontab already added"
-fi
+echo "$(crontab -l)" | grep "$BOOT_LAUNCH_SCRIPT" || {
+	crontab -l | { cat; echo "@reboot sh $BOOT_LAUNCH_SCRIPT &"; } | crontab -;
+}
